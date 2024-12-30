63f1d1c51b188a5218d66b93889410bfe83c97d0
Dhruvanayani9 <muvvadhruvanayani@gmail.com>
2024-12-30 10:29:54 +0530

    Add files via upload

---
+++ dhruva/src/nayani/manisha.java
@@ -0,0 +1,33 @@
+package nayani;
+
+public class manisha {
+	int age=10;
+	String name="NIT";
+			int id=597;
+
+	public static void main(String[] args) {
+		System.out.println("NIT");
+		manisha global=new manisha();
+		global.age();
+		global.name();
+		global.id();
+		// TODO Auto-generated method stub
+
+	}
+
+	private void id() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void name() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void age() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
